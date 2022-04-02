46e91d1456436ef79fe35db787075465e7da1c7c
flooprea <opreaf8@gmail.com>
2022-04-02 11:18:40 +0100

    Commit draft project

---
+++ src/main/java/com/example/prospectron/ProspectronApplication.java
@@ -0,0 +1,13 @@
+package com.example.prospectron;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProspectronApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProspectronApplication.class, args);
+	}
+
+}
